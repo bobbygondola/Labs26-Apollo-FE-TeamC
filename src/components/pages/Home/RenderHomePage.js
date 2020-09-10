c35961862e2aedc16c711b5ea8b7328caec0cd05
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-09-10 15:32:14 -0700

    changed colors in override

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Layout, Menu } from 'antd';
+const { Header, Content } = Layout;
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
   return (
-    <div>
-      <h1>Hi {userInfo.name} Welcome to Labs Basic SPA</h1>
-      <div>
+    <Layout className="layout">
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+        <h1 style={{ color: 'white' }}>
+          Hi {userInfo.name} Welcome to Labs Basic SPA
+        </h1>
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item key={1}>
+            <Link to="/new-topic">
+              <Button type="dashed">Add New Topic</Button>
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content>
         <p>
           This is an example of a common example of how we'd like for you to
           approach components.
@@ -18,18 +30,18 @@ function RenderHomePage(props) {
         <p>
           <Link to="/example-list">Example List of Items</Link>
         </p>
-        <p>
+        {/* <p>
           <Link to="/new-topic">
             <Button type="dashed">Add New Topic</Button>
           </Link>
-        </p>
+        </p> */}
         <p>
           <Button type="primary" onClick={() => authService.logout()}>
             Logout
           </Button>
         </p>
-      </div>
-    </div>
+      </Content>
+    </Layout>
   );
 }
 export default RenderHomePage;
--- src/styles/theme-overrides.js
+++ src/styles/theme-overrides.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '@primary-color': '#ec3944',
+  '@primary-color': '#00617e',
   '@link-color': ' #ec3944',
   '@success-color': '#52c41a',
   '@warning-color': '#faad14',
@@ -7,8 +7,8 @@ module.exports = {
   '@font-size-base': '14px',
   '@heading-color': 'rgba(0, 0, 0, 0.85)',
   '@text-color': 'rgba(0, 0, 0, 0.65)',
-  '@text-color-secondary': 'rgba(0, 0, 0, 0.45)',
-  '@disabled-color': 'rgba(0, 0, 0, 0.25)',
+  '@text-color-secondary': '#ffe000',
+  '@disabled-color': '#034051',
   '@border-radius-base': '2px',
   '@border-color-base': '#d9d9d9',
   '@box-shadow-base':
