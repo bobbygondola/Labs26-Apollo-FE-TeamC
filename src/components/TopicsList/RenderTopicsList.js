75fc1c927bb7513dd9348f638cc8794bc5fd4a8a
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-09-14 11:14:32 -0700

    Added TopicsList Component, no styling at this point, only dummy data

--- src/components/TopicsList/RenderTopicsList.js
+++ src/components/TopicsList/RenderTopicsList.js
@@ -1,42 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Avatar, Divider, Tooltip } from 'antd';
-import { UserOutlined, AntDesignOutlined } from '@ant-design/icons';
+import { Avatar, Tooltip } from 'antd';
+import {
+  UserOutlined,
+  AntDesignOutlined,
+  BellOutlined,
+} from '@ant-design/icons';
 
-const RenderTopicsList = props => {
-  return (
-    <div>
-      {props.data.map(topic => (
-        <figure key={topic.id}>
-          <Avatar.Group>
-            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+const topicsPlaceholder = [
+  {
+    id: 1,
+    title: 'Labs 26 Team C',
+    frequency: 'Daily', //should be daily, weekly, monthly, etc,
+    members: ['member1', 'member2', 'member3', 'etc'],
+    notifications: 5,
+  },
+  {
+    id: 2,
+    title: 'Weekly Status',
+    frequency: 'Weekly', //should be daily, weekly, monthly, etc,
+    members: ['member1', 'member2', 'member3', 'etc'],
+    notifications: 2,
+  },
+  {
+    id: 3,
+    title: 'Project Feedback',
+    frequency: 'Quarterly', //should be daily, weekly, monthly, etc,
+    members: ['member1', 'member2', 'member3', 'etc'],
+    notifications: 20,
+  },
+];
+
+const RenderTopicsList = props => (
+  <div>
+    {/* // {props.data.map(topic => ( */}
+    {topicsPlaceholder.map(topic => (
+      <div key={topic.id}>
+        <div>
+          <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+          <Avatar
+            style={{
+              backgroundColor: '#f56a00',
+            }}
+          >
+            K
+          </Avatar>
+          <Tooltip title="Ant User" placement="top">
             <Avatar
               style={{
-                backgroundColor: '#f56a00',
+                backgroundColor: '#87d068',
               }}
-            >
-              K
-            </Avatar>
-            <Tooltip title="Ant User" placement="top">
-              <Avatar
-                style={{
-                  backgroundColor: '#87d068',
-                }}
-                icon={<UserOutlined />}
-              />
-            </Tooltip>
-            <Avatar
-              style={{
-                backgroundColor: '#1890ff',
-              }}
-              icon={<AntDesignOutlined />}
+              icon={<UserOutlined />}
             />
-          </Avatar.Group>
-        </figure>
-      ))}
-    </div>
-  );
-};
+          </Tooltip>
+          <Avatar
+            style={{
+              backgroundColor: '#1890ff',
+            }}
+            icon={<AntDesignOutlined />}
+          />
+        </div>
+        <h3>{topic.frequency}</h3>
+        <h3>
+          <BellOutlined />
+          {topic.notifications}
+        </h3>
+      </div>
+    ))}
+  </div>
+);
 
 export default RenderTopicsList;
 
@@ -46,7 +78,7 @@ RenderTopicsList.propTypes = {
       id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
       title: PropTypes.string,
       frequency: PropTypes.string, //should be daily, weekly, monthly, etc
-      thumbnailUrl: PropTypes.string,
+      notifications: PropTypes.number,
     })
   ).isRequired,
 };
--- src/components/TopicsList/TopicsListContainer.js
+++ src/components/TopicsList/TopicsListContainer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { List } from '../common';
+import { getExampleData } from '../../api';
 import RenderTopicsList from './RenderTopicsList';
 
 const TopicsListContainer = () => {
   return (
     <List
       //axios request goes here
-      getItemsData={'PLACEHOLDER'}
+      getItemsData={getExampleData}
       LoadingComponent={() => <div>Loading Topics...</div>}
       RenderItems={RenderTopicsList}
     />
