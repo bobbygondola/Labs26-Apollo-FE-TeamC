364234c093eb6b798556816861f7576f08ddcbd3
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-09-15 12:48:45 -0700

    added component file for topic context slideout

---
+++ src/components/TopicContextSlideout/RenderTopicContextSlideout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RenderContextRadio from '../pages/NewTopicModal/ContextPages/RenderContextRadio';
+
+const RenderTopicContextSlideout = props => {
+  return <div style={{ display: 'flex', flexDirection: 'vertical' }}></div>;
+};
+
+export default RenderTopicContextSlideout;
---
+++ src/components/TopicContextSlideout/TopicContextSlideoutContainer.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import RenderTopicContextSlideout from './RenderTopicContextSlideout';
+
+const dummyData = {
+  topic_id: 1,
+  name: 'Labs 26 Team A',
+  date: '9/15/2020',
+  members: [
+    {
+      member_id: 1,
+      name: 'Dave',
+      role: 'Admin',
+    },
+    {
+      member_id: 2,
+      name: 'Doug',
+      role: 'Marketing',
+    },
+    {
+      member_id: 3,
+      name: 'Dan',
+      role: 'IT',
+    },
+  ],
+  questions_with_context: [
+    {
+      q_id: 1,
+      question: 'What are you doing this weekend?',
+      context:
+        'This is asked to check in with the team and see what fun stuff they have going on.',
+    },
+    {
+      q_id: 2,
+      question: 'What kind of car do you drive?',
+      context: 'It is crucial that we know what kind of wheels you have.',
+    },
+    {
+      q_id: 3,
+      question: 'What is your freshest outfit?',
+      context: 'We need the team to look fresh at all times.',
+    },
+  ],
+};
+
+const TopicContextSlideoutContainer = () => {
+  return <RenderTopicContextSlideout data={dummyData} />;
+};
+
+export default TopicContextSlideoutContainer;
